24399dd8f02f9a8f162844788c249b6a0414407f
Germán A. Ramírez Vela <german.ca13@gmail.com>
2018-10-13 10:23:59 -0500

    Rellocating ASP.NET MVC projects to a new Repository.

--- MVC/WebSanaAssessment/WebSanaAssessment/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSanaAssessment.MvcApplication" Language="C#" %>
--- MVC/WebSanaAssessment/WebSanaAssessment/Models/ModelDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace WebSanaAssessment.Models
-{
-   public class ModelDbContext : DbContext
-   {
-      public DbSet<ProductModel> Product { get; set; }
-   }
-}
